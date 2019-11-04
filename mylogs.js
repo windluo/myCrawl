538459ab3f530a496c9451ab46501092656baa37
luolixiong <luo19302@163.com>
2019-11-04 16:26:54 +0800

    更新

--- mylogs.js
+++ mylogs.js
@@ -4,6 +4,8 @@
 
 const fs = require("fs");
 
+// TODO：日志文件应该按照时间节点创建，比如每天抓一次，应该每天创建一个日志文件
+// 加入当天没有日志，其实可以不创建日志文件
 const writeStream = fs.createWriteStream("./logs/errors.log");
 
 // target 计划传入抓取失败的数据的详细信息，以便日志记录更详细
